8c40d174823e1c1ec92ffd586b3bee9856f7b383
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-03-02 07:00:16 +0100

    Initial Commit

---
+++ iBudget/iBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBudgetApp.swift
+//  iBudget
+//
+//  Created by Kiko on 2023-03-02.
+//
+
+import SwiftUI
+
+@main
+struct iBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
